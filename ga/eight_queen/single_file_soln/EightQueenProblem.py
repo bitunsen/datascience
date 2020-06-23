3876ec7fee42dc37f3439ee09ae56eb45c034dd8
Bitun Sen <bitunsen@gmail.com>
2020-06-23 00:48:59 -0400

    Adding more test cases

--- ga/eight_queen/single_file_soln/EightQueenProblem.py
+++ ga/eight_queen/single_file_soln/EightQueenProblem.py
@@ -151,7 +151,6 @@ class Chromosome:
         self._gene_list[_index_2_of_mutation] = point
 
 
-
 def get_all_attacking_points_from_one_point(x_cor, y_cor, num_of_queens):
     _invalid_point_arrays = []
     for i in range(num_of_queens):
@@ -174,7 +173,7 @@ def get_all_attacking_points_from_one_point(x_cor, y_cor, num_of_queens):
     return _invalid_point_arrays
 
 
-class GAExecution:
+class GAContext:
     _attacking_point_dictionary = {}
     _size_of_population = 0
     _length_of_chromosome = 0
@@ -203,11 +202,20 @@ class GAExecution:
         self._valid_gene_data = [i for i in range(num_of_queens)]
         self.__populate_attacking_point_dictionary()
 
+    def get_size_of_population(self):
+        return self._size_of_population
+
+    def get_valid_gene_data(self):
+        return self._valid_gene_data
+
     def get_current_generation(self):
         return self._current_generation
 
     def set_current_generation(self, cur_generation):
         self._current_generation = cur_generation
+
+    def get_num_of_queens(self):
+        return self._length_of_chromosome
 
     def __populate_attacking_point_dictionary(self):
         num_of_queens = self._length_of_chromosome
@@ -222,74 +230,15 @@ class GAExecution:
         key = str(x_cor) + "-" + str(y_cor)
         return self._attacking_point_dictionary[key]
 
-    """
-    This method generates '_size_of_population' number of Chromosomes
-    as part of initial population. 
-    """
-    def generate_initial_population(self):
-
-        self._current_generation = 1
-        for count_population in range(self._size_of_population):
-            # print("Valid Data Array :: ", self._valid_gene_data)
-            gene_array = self._valid_gene_data[:]
-            random.shuffle(gene_array)
-            # print("Gene Array :: ", gene_array)
-            _initial_chromosome = Chromosome(gene_array)
-            _chromosome_score = self.calculate_fitness_score(_initial_chromosome)
-            self._population_pool.append(_initial_chromosome)
-
-        # Sorting (in ascending order) all the chromosomes present in the population pool
-        self._population_pool = sorted(self._population_pool, key=lambda element: element.fitness_score,
-                                       reverse=True)
-        # for _element in self._population_pool:
-        #    _element.print_chromosome("Initial Pool :: ")
-
-        # print("-------------- END OF POPULATION GENERATION AND SORTING BASED ON SCORE --------------------------")
-        return self._population_pool
-
     def get_population_pool_size(self):
         return len(self._population_pool)
 
-    def calculate_fitness_score(self, input_chromosome):
-        if not isinstance(input_chromosome, Chromosome):
-            raise TypeError("Invalid argument - Only object of type Chromosome is expected.")
+    def get_population_pool(self):
+        return self._population_pool
 
-        _data = input_chromosome.get_gene_sequence()[:]
-        _non_attacking_queen = [0 for i in range(self._length_of_chromosome)]
-        for index in range(len(_data)):
-            current_point = Gene(index, _data[index])
-            # current_point.print("Current Queen :: ")
-            attacking_queen = 0
-            current_attacking_points = self.get_all_attacking_points_from_dictionary(current_point)
-            for i in range(len(_data)):
-                if i == index:
-                    continue
-                other_queen = Gene(i, _data[i])
-                for attacking_point in current_attacking_points:
-                    if other_queen.equals(attacking_point):
-                        attacking_queen = attacking_queen + 1
-                        # other_queen.print("Attacking Queen :: ")
-
-            _non_attacking_queen[index] = ((len(_data) - 1) - attacking_queen)
-            # print(non_attacking_queen)
-            input_chromosome.fitness_score = np.sum(_non_attacking_queen)
-        return input_chromosome.fitness_score
-
-    # This method assumes that the chromosome pool is already sorted
-    # Take half of the high scored parents for producing next generation kids
-    def run_parent_selection_process(self):
-        _max_score = 0
-        _score_array = []
-        _selected_parent_pool = []
-        _selected_parent_index_set = set()
-        initial_pool_size = len(self._population_pool)
-        _selected_parent_pool_size = int(self._size_of_population / 2)
-
-        if _selected_parent_pool_size % 2 != 0:
-            _selected_parent_pool_size += 1
-
-        _selected_parent_pool = self._population_pool[0:_selected_parent_pool_size]
-        return _selected_parent_pool, _max_score
+    def initialize_population_pool(self, _input_population_pool):
+        self._population_pool = []
+        self._population_pool = _input_population_pool[:]
 
     # Select parents using tournament process
     # Using Tournament process, Only half of the parents will be selected for reproduction
@@ -340,62 +289,133 @@ class GAExecution:
 
         return _selected_parent_pool, _max_score
 
-    def execute_crossover_mutation(self, _selected_parent_pool):
-        _num_of_selected_parents = len(_selected_parent_pool)
-        _mating_pool = []
-        # print("-------------- START OF CROSSOVER AND MUTATION OF PARENT SELECTION --------------------------")
-        for _index in range(_num_of_selected_parents):
-            _index_1 = _index % _num_of_selected_parents
-            _index_2 = (_index + 1) % _num_of_selected_parents
-            _mating_pool.append(_selected_parent_pool[_index_1])
-            _mating_pool.append(_selected_parent_pool[_index_2])
 
-        # for _chromosome in _mating_pool:
-        #    _chromosome.print_chromosome("[GEN - " + str(self._current_generation) + "] :: Matting Parent :: ")
+"""
+This method generates '_size_of_population' number of Chromosomes
+as part of initial population. 
+"""
 
-        self._population_pool = []
-        for _index in range(int(len(_mating_pool) / 2)):
-            _parent_1 = _mating_pool[(2 * _index)]
-            _parent_2 = _mating_pool[(2 * _index) + 1]
-            _child_1, _child_2 = _parent_1.crossover(_parent_2)
 
-            if _child_1.get_mutate_ratio() < 0.6:
-                _child_1.mutation()
-            if _child_2.get_mutate_ratio() < 0.6:
-                _child_2.mutation()
+def generate_initial_population(_context):
+    _context.set_current_generation(1)
+    for count_population in range(_context.get_size_of_population()):
+        # print("Valid Data Array :: ", self._valid_gene_data)
+        gene_array = _context.get_valid_gene_data()[:]
+        random.shuffle(gene_array)
+        # print("Gene Array :: ", gene_array)
+        _initial_chromosome = Chromosome(gene_array)
+        _chromosome_score = calculate_fitness_score(_context, _initial_chromosome)
+        _context.get_population_pool().append(_initial_chromosome)
 
-            self.calculate_fitness_score(_child_1)
-            self.calculate_fitness_score(_child_2)
-            self._population_pool.append(_child_1)
-            self._population_pool.append(_child_2)
+    # Sorting (in ascending order) all the chromosomes present in the population pool
+    _context.initialize_population_pool(sorted(_context.get_population_pool(),
+                                               key=lambda element: element.fitness_score, reverse=True))
+    # for _element in self._population_pool:
+    #    _element.print_chromosome("Initial Pool :: ")
 
-        # print("-------------- AFTER CROSSOVER AND MUTATION AND BEFORE SORTING OF "
-        #      "PARENT SELECTION --------------------------")
+    # print("-------------- END OF POPULATION GENERATION AND SORTING BASED ON SCORE --------------------------")
+    return _context.get_population_pool()
 
-        # for _chromosome in self._population_pool:
-        #    _chromosome.print_chromosome("[GEN - " + str(self._current_generation)
-        #    + "] :: Before Sorting New Child :: ")
 
-        # Sorting (in ascending order) all the chromosomes present in the population pool
-        self._population_pool = sorted(self._population_pool, key=lambda element: element.fitness_score,
-                                       reverse=True)
-        # print("-------------- AFTER CROSSOVER AND MUTATION AND SORTING OF PARENT SELECTION
-        # --------------------------")
+def calculate_fitness_score(_context, input_chromosome):
+    if not isinstance(input_chromosome, Chromosome):
+        raise TypeError("Invalid argument - Only object of type Chromosome is expected.")
 
-        # for _chromosome in self._population_pool:
-        #    _chromosome.print_chromosome("[GEN - " + str(self._current_generation) + "] :: New Child :: ")
+    _data = input_chromosome.get_gene_sequence()[:]
+    _non_attacking_queen = [0 for i in range(_context.get_num_of_queens())]
+    for index in range(len(_data)):
+        current_point = Gene(index, _data[index])
+        # current_point.print("Current Queen :: ")
+        attacking_queen = 0
+        current_attacking_points = _context.get_all_attacking_points_from_dictionary(current_point)
+        for i in range(len(_data)):
+            if i == index:
+                continue
+            other_queen = Gene(i, _data[i])
+            for attacking_point in current_attacking_points:
+                if other_queen.equals(attacking_point):
+                    attacking_queen = attacking_queen + 1
+                    # other_queen.print("Attacking Queen :: ")
 
-        return self._population_pool
+        _non_attacking_queen[index] = ((len(_data) - 1) - attacking_queen)
+        # print(non_attacking_queen)
+        input_chromosome.fitness_score = np.sum(_non_attacking_queen)
+    return input_chromosome.fitness_score
+
+
+# This method assumes that the chromosome pool is already sorted
+# Take half of the high scored parents for producing next generation kids
+def run_parent_selection_process(_context):
+    _max_score = 0
+    _score_array = []
+    _selected_parent_pool = []
+    _selected_parent_index_set = set()
+    initial_pool_size = len(_context.get_population_pool())
+    _selected_parent_pool_size = int(_context.get_size_of_population() / 2)
+
+    if _selected_parent_pool_size % 2 != 0:
+        _selected_parent_pool_size += 1
+
+    _selected_parent_pool = _context.get_population_pool()[0:_selected_parent_pool_size]
+    return _selected_parent_pool, _max_score
+
+
+def execute_crossover_mutation(_context, _selected_parent_pool):
+    _num_of_selected_parents = len(_selected_parent_pool)
+    _mating_pool = []
+    # print("-------------- START OF CROSSOVER AND MUTATION OF PARENT SELECTION --------------------------")
+    for _index in range(_num_of_selected_parents):
+        _index_1 = _index % _num_of_selected_parents
+        _index_2 = (_index + 1) % _num_of_selected_parents
+        _mating_pool.append(_selected_parent_pool[_index_1])
+        _mating_pool.append(_selected_parent_pool[_index_2])
+
+    # for _chromosome in _mating_pool:
+    #    _chromosome.print_chromosome("[GEN - " + str(self._current_generation) + "] :: Matting Parent :: ")
+
+    _children_population_pool = []
+    for _index in range(int(len(_mating_pool) / 2)):
+        _parent_1 = _mating_pool[(2 * _index)]
+        _parent_2 = _mating_pool[(2 * _index) + 1]
+        _child_1, _child_2 = _parent_1.crossover(_parent_2)
+
+        if _child_1.get_mutate_ratio() < 0.6:
+            _child_1.mutation()
+        if _child_2.get_mutate_ratio() < 0.6:
+            _child_2.mutation()
+
+        calculate_fitness_score(_context, _child_1)
+        calculate_fitness_score(_context, _child_2)
+        _children_population_pool.append(_child_1)
+        _children_population_pool.append(_child_2)
+
+    # print("-------------- AFTER CROSSOVER AND MUTATION AND BEFORE SORTING OF "
+    #      "PARENT SELECTION --------------------------")
+
+    # for _chromosome in self._population_pool:
+    #    _chromosome.print_chromosome("[GEN - " + str(self._current_generation)
+    #    + "] :: Before Sorting New Child :: ")
+
+    # Sorting (in ascending order) all the chromosomes present in the population pool
+    _context.initialize_population_pool(sorted(_children_population_pool, key=lambda element: element.fitness_score,
+                                               reverse=True))
+    # print("-------------- AFTER CROSSOVER AND MUTATION AND SORTING OF PARENT SELECTION
+    # --------------------------")
+
+    # for _chromosome in self._population_pool:
+    #    _chromosome.print_chromosome("[GEN - " + str(self._current_generation) + "] :: New Child :: ")
+
+    return _context.get_population_pool()
 
 
 if __name__ == "__main__":
 
     number_of_queens = 8
-    engine = GAExecution(100, number_of_queens, 0.01)
+    execution_context = GAContext(100, number_of_queens, 0.01)
     current_generation = 1
     _max_generation = 2000
     target_chromosome_array = [Chromosome([0 for i in range(number_of_queens)])]
-    _initial_population_pool = engine.generate_initial_population()
+    _initial_population_pool = generate_initial_population(execution_context)
     _max_scored_chromosome = _initial_population_pool[0]
     _max_possible_score = number_of_queens * (number_of_queens - 1)
 
@@ -406,11 +426,11 @@ if __name__ == "__main__":
     else:
 
         while True:
-            selected_parent_pool, max_score = engine.run_parent_selection_process()
-            _initial_population_pool = engine.execute_crossover_mutation(selected_parent_pool)
+            selected_parent_pool, max_score = run_parent_selection_process(execution_context)
+            _initial_population_pool = execute_crossover_mutation(execution_context, selected_parent_pool)
             _max_scored_chromosome = _initial_population_pool[0]
             current_generation = current_generation + 1
-            engine.set_current_generation(current_generation)
+            execution_context.set_current_generation(current_generation)
 
             if _max_scored_chromosome.get_fitness_score() == _max_possible_score:
                 target_chromosome_array[0] = _max_scored_chromosome
